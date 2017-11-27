3da6c615e0327cc3cc417414afab7ac8987a3386
Cohen, Jacob <Jacob.Cohen@scientificgames.com>
2017-11-27 17:40:25 +0200

    Initial Commit

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+	void OnTriggerEnter (Collider col)
+	{
+		Debug.Log("Ball Hit The Water");
+		GameManager.instance.LoseLife();
+	}
+}
---
+++ Assets/Scripts/DestroyOverTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOverTime : MonoBehaviour
+{
+
+	public float DestroyTime = 1f;
+
+	private void Start()
+	{
+		Destroy(gameObject, DestroyTime);
+	}
+}
